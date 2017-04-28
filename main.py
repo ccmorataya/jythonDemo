08e858606d83a9803354517a9b1efa7454eb1715
Cristian Morataya <cris.morataya@gmail.com>
2017-04-27 22:42:30 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from javax.swing import JFrame, JButton
+
+frame = JFrame("Title screen", size=(300, 100), defaultCloseOperation=JFrame.EXIT_ON_CLOSE)
+
+def on_click(event):
+    print "You click me"
+    print event
+
+boton = JButton("click me", actionPerformed=on_click)
+frame.add(boton)
+frame.visible = True
+
