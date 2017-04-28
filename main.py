24fa9f8be6cac4a96ec9d37281c354a7b1ccb10a
Cristian Morataya <cris.morataya@gmail.com>
2017-04-27 22:58:08 -0600

    Reformat code

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from javax.swing import JFrame, JButton
 
-frame = JFrame("Title screen", size=(300, 100), defaultCloseOperation=JFrame.EXIT_ON_CLOSE)
+frame = JFrame("Title screen",
+               size=(300, 100),
+               defaultCloseOperation=JFrame.EXIT_ON_CLOSE
+               )
+
 
 def on_click(event):
     print "You click me"
 
+
 boton = JButton("click me", actionPerformed=on_click)
 frame.add(boton)
 frame.visible = True
-
